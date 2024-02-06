6d7cfb62465dc65a7351a6e279ef0ec37b4e22f6
BigHeadliner <bigheadliner@gmail.com>
2024-02-06 21:12:47 +0200

    online

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,  
+    responsive: [
+      {
+        breakpoint: 1449,
+        settings: {
+          slidesToShow: 3, 
+        }
+      }
+    ]
+  });
+
+
+});
+          
+       
+     
+  
+  
+  
+  
+     
+
+ 
+
